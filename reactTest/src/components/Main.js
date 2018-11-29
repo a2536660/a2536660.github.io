1f5f4aa3da39d3bf4f23f9c7b44e2be1a9197273
徐兴 <a2536660@163.com>
2018-11-29 16:36:33 +0800

    1

---
+++ reactTest/src/components/Main.js
@@ -0,0 +1,27 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+
+let yeomanImage = require('../images/yeoman.png');
+let iconImage = require('../images/Icon-180.png');
+
+class AppComponent extends React.Component {
+
+  render() {
+    return (
+      <div className="index">
+        <img src={yeomanImage} alt="Yeoman Generator" />
+        <img src={iconImage} alt="icon" />
+        <span>hellow 22s1221</span>
+        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
